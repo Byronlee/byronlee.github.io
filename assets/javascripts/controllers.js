2ff23b08612c6cef20e82623625840dce3c44868
Byronlee <lbyronlee@gmail.com>
2014-05-26 19:19:38 +0000

    add fontawesome

--- assets/javascripts/controllers.js
+++ assets/javascripts/controllers.js
@@ -1,11 +1,12 @@
 function ShowCaseListCtrl($scope) {
     $scope.cases = [
         {"content": "A woman programmer husband to let him go to the store to buy things: you go to a nearby store to buy some bread, eggs if there is, then, to buy six back, the husband bought a six bread back, his wife was shocked: Why did you buy 6 bread? ! Programmer husband replied: because they have eggs."},
-        {"content": "&nbsp;&nbsp;&nbsp;&nbsp;在那山的这边海的那边有一群程序员，他们老实又胹腆，他们聪明又没钱。他们一天到晚坐在那里熬夜写软件，饿了就咬一口方便面～～哦苦命的程序员，哦苦命的程序员，只要一改需求他们就要重新搞一遍，但是期限只剩下两天"}
+        {"content": "     在那山的这边海的那边有一群程序员，他们老实又胹腆，他们聪明又没钱。他们一天到晚坐在那里熬夜写软件，饿了就咬一口方便面～～哦苦命的程序员，哦苦命的程序员，只要一改需求他们就要重新搞一遍，但是期限只剩下两天"}
     ];
 }
 
-
-function GreetingController($scope) {
-    $scope.greeting = 'Hola!';
-}
+angular.module('myApp', []).config([
+    '$interpolateProvider', function($interpolateProvider) {
+        return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+    }
+]);
