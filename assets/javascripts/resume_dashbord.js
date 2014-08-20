6d0bb3107aaab8654352730b60fdca05823c7e51
Byronlee <lbyronlee@gmail.com>
2014-08-20 10:45:39 +0000

    修改纸质简历获取方式

--- assets/javascripts/resume_dashbord.js
+++ assets/javascripts/resume_dashbord.js
@@ -1,7 +1,7 @@
 $(function () {
 
     // 右边终端效果
-    var html = '(● ◡ ●)ﾉ♥ 嗨，欢迎光临！我叫李江华，你想了解我的什么呢？\n用法：\n　　1.　简洁版简历\n　　2.　文艺版简历\n　　3.　github版简历\n　　4.　给我留言\n　　5.　回到博客首页\n请输入你想了解的编号：\n';
+    var html = '(● ◡ ●)ﾉ♥ 嗨，欢迎光临！我叫李江华，你想了解我的什么呢？\n用法：\n　　1.　简洁版简历\n　　2.　文艺版简历\n　　3.　github版简历\n　　4.　获取纸质简历（PDF格式，中文）\n　　5.　Getting paper resume（PDF，English）\n　　6.　给我留言\n　　7.　回到博客首页\n请输入你想了解的编号：\n';
     var jqconsole = $('#console').jqconsole(html, 'Ξ ~/ByronleeStyle git:(master) ▶ ', 'I like you! and you?');
 
     var startPrompt = function () {
@@ -18,16 +18,16 @@ $(function () {
                 case "3":
                     window.open("http://resume.github.io/?Byronlee")
                     break;
-               // case "4":
-            //        window.location.href="https://raw.githubusercontent.com/Byronlee/byronlee.github.io/master/resume/%E4%B8%AA%E4%BA%BA%E7%AE%80%E5%8E%86%E6%9D%8E%E6%B1%9F%E5%8D%8E%EF%BC%88%E4%B8%AD%E6%96%87%EF%BC%89.pdf"
-              //      break;
-            //    case "5":
-              //      window.location.href="https://raw.githubusercontent.com/Byronlee/byronlee.github.io/master/resume/Byronlee's%20resume%EF%BC%88English%EF%BC%89.pdf"
-                //    break;
                 case "4":
-                    alert("Please Email To Me: lbyronlee@gmail.com")
+                    window.location.href="http://p4.dfs.kuaipan.cn/cdlnode/dl/?ud=N2MiQ4a9ArYaZQ3qg9qUYHgE31sHL9IJ4RfjJnmY6j8~0-http:@@180.97.176.115@ufa_new@~DOwXdfHQpsuGmP4Yu)obCYVum1lRH3c-0-@0-AAAAAAAAAAAAAAAAAAAAAAAAAAA-u(IlcNHEZk)Ue6XuSguwbjODUZLASKNcNBDmCNle4fs-76189d5e50250c-0-0~1&src=12285642281065905-846871-MDAyYmE1YmE2MWUzNDdhYWI1MmE1ZGNjZGUyNzkxOWQtMTg1MDI5MDQyNw-&tm=1408502218&cip=171.216.71.45&bea=YXR0YWNobWVudDtmaWxlbmFtZT3kuKrkurrnroDljobmnY7msZ/ljY7vvIjkuK3mlofvvIkucGRmOw=="
                     break;
                 case "5":
+                    window.location.href="http://p3.dfs.kuaipan.cn/cdlnode/dl/?ud=HDqk5(rdGAMADEzY7s3yQghq)eDYzVp70(XYmcckg4Y~0-http:@@112.84.131.18@ufa_new@~BjT5NEiNzNxc7LmVDblhfgiRHiOsIys-0-@0-AAAAAAAAAAAAAAAAAAAAAAAAAAA-sgC5yNZ0tSEFolzq8TuK(nVzvRT9YannB(G3Ex2N6TE-76189d5250260c-0-0~1&src=12285642281065903-406777-MDAyYmE1YmE2MWUzNDdhYWI1MmE1ZGNjZGUyNzkxOWQtMTg1MDI5MDQyNw-&tm=1408502224&cip=171.216.71.45&bea=YXR0YWNobWVudDtmaWxlbmFtZT1CeXJvbmxlZSdzIHJlc3VtZe+8iEVuZ2xpc2jvvIkucGRmOw=="
+                    break;
+                case "6":
+                    alert("Please Email To Me: lbyronlee@gmail.com")
+                    break;
+                case "7":
                     window.open("/")
                     break;
                 default:
