65dea9b2cab02485c62d2e5a17b8bfae2701be8b
Byronlee <lbyronlee@gmail.com>
2014-08-20 11:12:37 +0000

    还原链接

--- assets/javascripts/resume_dashbord.js
+++ assets/javascripts/resume_dashbord.js
@@ -18,11 +18,11 @@ $(function () {
                 case "3":
                     window.open("http://resume.github.io/?Byronlee")
                     break;
-                case "4":
-                    window.location.href="http://p4.dfs.kuaipan.cn/cdlnode/dl/?ud=N2MiQ4a9ArYaZQ3qg9qUYMRdYUQDMniz5oLpY1XD8SU~0-http:@@180.97.176.115@ufa_new@~DOwXdfHQpsuGmP4Yu)obCYVum1lRH3c-0-@0-AAAAAAAAAAAAAAAAAAAAAAAAAAA-u(IlcNHEZk)Ue6XuSguwbjODUZLASKNcNBDmCNle4fs-76189d5e50250c-0-0~1&src=12285642281065905-846871-MDAyYmE1YmE2MWUzNDdhYWI1MmE1ZGNjZGUyNzkxOWQtMTg1MDI5MDQyNw-&tm=1408502647&cip=171.216.71.45&bea=YXR0YWNobWVudDtmaWxlbmFtZT3kuKrkurrnroDljobmnY7msZ/ljY7vvIjkuK3mlofvvIkucGRmOw=="
+               case "4":
+                    window.location.href="https://raw.githubusercontent.com/Byronlee/byronlee.github.io/master/resume/%E4%B8%AA%E4%BA%BA%E7%AE%80%E5%8E%86%E6%9D%8E%E6%B1%9F%E5%8D%8E%EF%BC%88%E4%B8%AD%E6%96%87%EF%BC%89.pdf"
                     break;
                 case "5":
-                    window.location.href="http://p3.dfs.kuaipan.cn/cdlnode/dl/?ud=HDqk5(rdGAMADEzY7s3yQghq)eDYzVp70(XYmcckg4Y~0-http:@@112.84.131.18@ufa_new@~BjT5NEiNzNxc7LmVDblhfgiRHiOsIys-0-@0-AAAAAAAAAAAAAAAAAAAAAAAAAAA-sgC5yNZ0tSEFolzq8TuK(nVzvRT9YannB(G3Ex2N6TE-76189d5250260c-0-0~1&src=12285642281065903-406777-MDAyYmE1YmE2MWUzNDdhYWI1MmE1ZGNjZGUyNzkxOWQtMTg1MDI5MDQyNw-&tm=1408502224&cip=171.216.71.45&bea=YXR0YWNobWVudDtmaWxlbmFtZT1CeXJvbmxlZSdzIHJlc3VtZe+8iEVuZ2xpc2jvvIkucGRmOw=="
+                    window.location.href="https://raw.githubusercontent.com/Byronlee/byronlee.github.io/master/resume/Byronlee's%20resume%EF%BC%88English%EF%BC%89.pdf"
                     break;
                 case "6":
                     alert("Please Email To Me: lbyronlee@gmail.com")
