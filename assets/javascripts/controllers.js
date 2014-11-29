d05bc467595f2cd5571f7b9263a925c11b8e3073
Byronlee <lbyronlee@gmail.com>
2014-11-30 00:29:08 +0800

    更新一次短语

--- assets/javascripts/controllers.js
+++ assets/javascripts/controllers.js
@@ -2,15 +2,20 @@ function ShowCaseListCtrl($scope) {
     $scope.cases = [
         ["text"],
      //  ["demo"],
-        ["image"],
-        ["code"]
+        ["image"]//,
+     //   ["code"]
     ];
 
     $scope.texts  = [
        // ["A woman programmer husband to let him go to the store to buy things: you go to a nearby store to buy some bread, eggs if there is, then, to buy six back, the husband bought a six bread back, his wife was shocked: Why did you buy 6 bread? ! Programmer husband replied: because they have eggs."],
        // ["在那山的这边海的那边有一群程序员，他们老实又胹腆，他们聪明又没钱。他们一天到晚坐在那里熬夜写软件，饿了就咬一口方便面～～哦苦命的程序员，哦苦命的程序员，只要一改需求他们就要重新搞一遍，但是期限只剩下两天"]
-          ["一切都是最好的安排！"],
-          ["在汽车时代早期，你问客户要什么，很多人可能都会回答'要一匹跑的更快的马'"]
+       //   ["一切都是最好的安排！"],
+          ["也许你现在乃是一个人吃饭，一个人看电影，一个人睡觉，一个人乘地铁，然而你却能一个人吃饭，一个人看电影，一个人睡觉，一个人乘地铁，很多人离开另外一个人就没有了自己，而你却一个人度过了所有，你的孤独，虽败犹荣！"],
+          ["在汽车时代早期，你问客户要什么，很多人可能都会回答'要一匹跑的更快的马'"],
+          ["但行好事，莫问前程"],
+          ["泰山崩于前，我依然沐浴更衣焚香沏茶，诚心正意，手起键落： Hello, World!"],
+          ["生命中遇到的每一本书，都不是偶然"],
+          ["北方有佳人，绝世而独立。一顾倾人城，再顾倾人国"]
     ];
 
     $scope.images = [
@@ -21,8 +26,10 @@ function ShowCaseListCtrl($scope) {
       //  ["/assets/images/dashborad/myhome.jpg"]
       //  ["/assets/images/dashborad/biye.jpg"]
 
-        ["/assets/images/dashborad/2014-12/1.jpg"],
-        ["/assets/images/dashborad/2014-12/3.jpg"]
+        ["/assets/images/dashborad/2014-12/1.jpeg"],
+        ["/assets/images/dashborad/2014-12/2.jpeg"],
+        ["/assets/images/dashborad/2014-12/3.jpeg"],
+        ["/assets/images/dashborad/2014-12/4.jpeg"]
     ];
 
     $scope.demos  = [
