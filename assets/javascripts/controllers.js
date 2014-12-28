45538a43a415c7f154fa5b6996cfe59e43fe8b76
Byronlee <lbyronlee@gmail.com>
2014-12-28 11:52:09 +0800

    update index

--- assets/javascripts/controllers.js
+++ assets/javascripts/controllers.js
@@ -1,9 +1,9 @@
 function ShowCaseListCtrl($scope) {
     $scope.cases = [
-      //  ["text"],
+        ["text"],
      //  ["demo"],
-      //  ["image"]//,
-        ["code"]
+        ["image"],
+      //  ["code"]
     ];
 
     $scope.texts  = [
@@ -11,7 +11,7 @@ function ShowCaseListCtrl($scope) {
        // ["在那山的这边海的那边有一群程序员，他们老实又胹腆，他们聪明又没钱。他们一天到晚坐在那里熬夜写软件，饿了就咬一口方便面～～哦苦命的程序员，哦苦命的程序员，只要一改需求他们就要重新搞一遍，但是期限只剩下两天"]
        //   ["一切都是最好的安排！"],
           ["也许你现在乃是一个人吃饭，一个人看电影，一个人睡觉，一个人乘地铁，然而你却能一个人吃饭，一个人看电影，一个人睡觉，一个人乘地铁，很多人离开另外一个人就没有了自己，而你却一个人度过了所有，你的孤独，虽败犹荣！"],
-          ["在汽车时代早期，你问客户要什么，很多人可能都会回答'要一匹跑的更快的马'"],
+          //["在汽车时代早期，你问客户要什么，很多人可能都会回答'要一匹跑的更快的马'"],
           ["但行好事，莫问前程"],
           ["泰山崩于前，我依然沐浴更衣焚香沏茶，诚心正意，手起键落： Hello, World!"],
           ["生命中遇到的每一本书，都不是偶然"],
@@ -26,10 +26,11 @@ function ShowCaseListCtrl($scope) {
       //  ["/assets/images/dashborad/myhome.jpg"]
       //  ["/assets/images/dashborad/biye.jpg"]
 
-        ["/assets/images/dashborad/2014-12/1.jpeg"],
-        ["/assets/images/dashborad/2014-12/2.jpeg"],
-        ["/assets/images/dashborad/2014-12/3.jpeg"],
-        ["/assets/images/dashborad/2014-12/4.jpeg"]
+      //  ["/assets/images/dashborad/2014-12/1.jpeg"],
+      //  ["/assets/images/dashborad/2014-12/2.jpeg"],
+       // ["/assets/images/dashborad/2014-12/3.jpeg"],
+      //  ["/assets/images/dashborad/2014-12/4.jpeg"]
+        ["/assets/images/dashborad/2015-1/1.jpg"]
     ];
 
     $scope.demos  = [
